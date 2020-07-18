a7bede29f001e70ae20a5125b8357a688315df8a
n-pretorious <pretoriousndungu@gmail.com>
2020-07-18 13:21:40 +0300

    Add a reset button function to reset the paragraph of displayed akan name

--- js/script.js
+++ js/script.js
@@ -1,44 +1,49 @@
+let dob = document.getElementById("birthDate").value;
+let weekDay = new Date(dob).getDay()
 
-function akanName(){
-  let dob = document.getElementById("birthDate").value;
-  let weekDay = new Date(dob).getDay()
+let gender = document.getElementsByName("gender")
+let response = document.getElementById("response")
 
-  let gender = document.getElementsByName("gender");
-  let response = document.getElementById("response")
+// reset fields in the form
+function clearAnswer() {
+  response.innerHTML = ''
+}
 
+// find users akan name
+function akanName() {
   for (let i = 0, length = gender.length; i < length; i++) {
     if (gender[i].checked) {
-      if (gender[i].value == "male") {
+      if (gender[i].value == 'male') {
         if (weekDay === 0) {
-          response.innerHTML = "Kwasi"
+          response.innerHTML = 'Kwasi'
         } else if (weekDay === 1) {
-          response.innerHTML = "Kwadwo"
+          response.innerHTML = 'Kwadwo'
         } else if (weekDay === 2) {
-          response.innerHTML = "Kwabena"
+          response.innerHTML = 'Kwabena'
         } else if (weekDay === 3) {
-          response.innerHTML = "Kwaku"
+          response.innerHTML = 'Kwaku'
         } else if (weekDay === 4) {
-          response.innerHTML = "Yaw"
+          response.innerHTML = 'Yaw'
         } else if (weekDay === 5) {
-          response.innerHTML = "Kofi"
+          response.innerHTML = 'Kofi'
         } else {
-          response.innerHTML = "Kwame"
+          response.innerHTML = 'Kwame'
         }
       } else {
         if (weekDay === 0) {
-          response.innerHTML = "Akosua"
+          response.innerHTML = 'Akosua'
         } else if (weekDay === 1) {
-          response.innerHTML = "Adwoa"
+          response.innerHTML = 'Adwoa'
         } else if (weekDay === 2) {
-          response.innerHTML = "Abenaa"
+          response.innerHTML = 'Abenaa'
         } else if (weekDay === 3) {
-          response.innerHTML = "Akua"
+          response.innerHTML = 'Akua'
         } else if (weekDay === 4) {
-          response.innerHTML = "Yaa"
+          response.innerHTML = 'Yaa'
         } else if (weekDay === 5) {
-          response.innerHTML = "Afua"
+          response.innerHTML = 'Afua'
         } else {
-          response.innerHTML = "Ama"
+          response.innerHTML = 'Ama'
         }
       }
     }
